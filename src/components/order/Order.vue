7ad473c8533cbad006e20a2794cdc34a4027eead
Sophia <1412152623@qq.com>
2017-11-07 01:17:14 +0800

    12

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -2,27 +2,219 @@
   <div class="OrderPage">
   <HeaderTag page="order"></HeaderTag>
   <div class="order_content">
-      <div class="order_list">
-        <div class="line_light">
-           <div class="choose_list">
+      <div class="order_wrap">
+         <div class="service_list">
+              <h1>您需要的服务是？</h1>
                <ul class="cf">
-                  <li>选择服务类型</li>
-                  <li>选择设备型号</li>
-                  <li>确认故障类型</li>
-                  <li>给出解决方案</li>
-                  <li>微信扫描下单</li>
-               </ul>
-           </div>
-           <div class="white_line ">
-             <p class="orange_line step_one"></p>
-             <span class="point_one">1</span>
-             <span class="point_two">2</span>
-             <span class="point_three">3</span>
-             <span class="point_four">4</span>
-             <span class="point_five">5</span>
-           </div>
-        </div> 
-      </div>  
+                  <li>
+                      <div class="li_wrap cf">
+                          <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                          <div class="one"><img src="./pict/household.png"></div>
+                          <div class="two">
+                            <h3>家电维修</h3>
+                            <p>空调，冰箱，电视,燃气热水器，饮水机等</p>
+                          </div>     
+                      </div>
+                  </li>
+                  <li>
+                      <div class="li_wrap cf">
+                          <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                          <div class="one"><img src="./pict/furniture.png" height="86" width="86"></div>
+                          <div class="two">
+                            <h3>家具维修</h3>
+                            <p>门窗家具维修,灯具安装灯具/电路,等</p>
+                          </div>
+                      </div>
+                  </li>
+                  <li>
+                      <div class="li_wrap cf">
+                          <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                          <div class="one"><img src="./pict/bathroom.png" height="86" width="86"></div>
+                          <div class="two">
+                            <h3>卫浴洁具</h3>
+                            <p>龙头/管件,马桶,浴霸水路管件更换维修等</p>
+                          </div>   
+                      </div>
+                  </li>
+                  <li>
+                    <div class="li_wrap cf">
+                        <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                        <div class="one"><img src="./pict/equipment.png" height="86" width="86"></div>
+                        <div class="two">
+                          <h3 >智能设备</h3>
+                          <p>电脑/网络维修，网络调试电脑系统重装等</p>
+                        </div>  
+                    </div>
+                  </li>
+              </ul>
+         </div>
+          <div class="repair_content">
+               <h1>您需要维修的是？</h1>
+               <div>
+                 <ul>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>冰箱</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>电视</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>空调</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>洗衣机</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>电磁炉</span>
+                   </li>
+                    <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>电磁炉</span>
+                   </li>
+                    <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>电磁炉</span>
+                   </li>
+                    <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>电磁炉</span>
+                   </li>
+                    <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>电磁炉</span>
+                   </li>
+                 </ul>
+               </div> 
+          </div>
+           <div class="repair_content">
+               <h1>您的维修设备是？</h1>
+               <div>
+                 <ul>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>单门冰箱</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>双门冰箱</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>三门冰箱</span>
+                   </li>
+                    <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>三门冰箱</span>
+                   </li>
+                    <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>三门冰箱</span>
+                   </li>
+                    <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>三门冰箱</span>
+                   </li>
+                    <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>三门冰箱</span>
+                   </li>
+                     <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>三门冰箱</span>
+                   </li>
+                 </ul>
+               </div> 
+          </div>
+          <div class="repair_content">
+               <h1>您的设备问题是？</h1>
+               <div>
+                 <ul>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>不制冷</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>不亮灯</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>冰箱不启动</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>不制冷</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>不亮灯</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>冰箱不启动</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>不制冷</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>不亮灯</span>
+                   </li>
+                   <li>
+                    <div class="img_left dis"><img src="./pict/checked.png" height="40" width="40"></div>
+                    <span>冰箱不启动</span>
+                   </li>
+                 </ul>
+               </div> 
+          </div>
+          <div class="order_bottom">
+            <h1>根据你的设备问题，给出以下维修方案</h1>
+            <div class="bottom_list">
+                <table>
+                    <tr class="td_spe">
+                        <td>故障问题</td>
+                        <td>维修方案</td>
+                        <td>维修费用（元)</td>
+                    </tr>
+                    <tr>
+                        <td>冰箱不启动</td>
+                        <td>更换电池</td>
+                        <td class="spe_orange">￥400</td>
+                    </tr>
+                    <tr>
+                        <td>冰箱不启动</td>
+                        <td>更换电池</td>
+                        <td class="spe_orange">￥400</td>
+                    </tr>
+                    <tr>
+                        <td>冰箱不启动</td>
+                        <td>更换电池</td>
+                        <td class="spe_orange">￥400</td>
+                    </tr>
+                    <tr>
+                        <td>冰箱不启动</td>
+                        <td>更换电池</td>
+                        <td class="spe_orange">￥400</td>
+                    </tr>
+                    <tr>
+                      <td colspan="3"><input type="text" name=""></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="scan_place">
+                <div class="price"><span class="span_one">此次维修总价</span><span class="span_two">￥400</span></div>
+                <img src="./pict/scan_big.png" height="188" width="188">
+                <p>微信下单,岂止简单</p>
+                <p>让您随时关注订单动态</p>
+            </div> 
+          </div>
+      </div>
   </div>
 	<FooterTag></FooterTag>
 	<Floating></Floating>
@@ -53,71 +245,127 @@ export default {
 }
 .OrderPage .order_content{
   padding-top: 65px;
-}
-.OrderPage .order_content .order_list{
   width: 1200px;
-  height: 70px;
-  background-color: #4a6075;
   margin: 0 auto;
 }
-.OrderPage .order_content .order_list ul{width: 1115px;margin: 0 auto;}
-.OrderPage .order_content .order_list ul li{
-  display: inline-block;
-  color: #ffffff;
-  font-size: 14px;
-  line-height: 10px;
-  width: 220px;
-  text-align: center;
-  margin-top: 13px;
-
-}
-.OrderPage .order_content .order_list .white_line{
-  width: 880px;
-  height: 4px;
-  background-color: #fff;
-  margin: 0 auto;
+.OrderPage .order_content .service_list ul li{
+  float: left;
+  width: 281px;
+  height: 142px;
+  border: 1px solid #dddddd;
+  margin-bottom: 23px;
+  margin-right: 15px;
   margin-top: 22px;
-  position: relative;
 }
-.OrderPage .order_content .order_list .white_line .orange_line{
-  height: 4px;
-  background-color: #ea5413;
+.OrderPage .order_content .service_list ul li .li_wrap{
+  border-bottom: 1px solid #ea5514;
+  height: 141px;
+ /* background: url(./pict/checked.png) no-repeat;*/
+}
+
+.OrderPage .order_content .service_list ul li .one,.OrderPage .order_content .service_list ul li .two{float: left;}
+.OrderPage .order_content .service_list ul li{
+  position: relative; 
+  cursor: pointer;
+}
+.OrderPage .order_content .service_list ul li .two{
+  width: 140px;
+  position: absolute;
+  right: 9px;
+  top: 42px;
+}
+.OrderPage .order_content .service_list ul li .one{
+  margin-left: 18px;
+  margin-top: 32px;
+}
+.OrderPage .order_content  .dis{
+display: none;
+position: absolute;
+left: 0;top:0;
+}
+.OrderPage .order_content .service_list ul li h3{
+  font-size: 20px;
+  color: #333333;
+  font-weight: normal;
+}
+.OrderPage .order_content .service_list ul li p{
+  font-size: 14px;
+  color: #666666;
+}
+.OrderPage .order_content .order_wrap h1{
+  font-size: 24px;
+  color: #333333;
+  font-weight: normal;
+}
+.OrderPage .order_content .order_wrap .repair_content ul li{
+  display: inline-block;
+  width: 219px;
+  height: 44px;
+  margin-right: 14px;
+  border: 1px solid  #dddddd;
+  position: relative;
+  margin-bottom: 5px
+}
+.OrderPage .order_content .order_wrap .repair_content ul{margin-bottom: 22px;margin-top: 21px;}
+.OrderPage .order_content .order_wrap .repair_content ul li span{
+  font-size: 16px;
+  color: #333333;
+  position: absolute;
+  left: 94px;
+  top: 11px;
+}
+.OrderPage .order_content .order_wrap .order_bottom .bottom_list{
+  margin-top: 51px;
+}
+
+.OrderPage .order_content .order_wrap .order_bottom .bottom_list td{
+  width: 400px;
+  text-align: center;
+  padding-bottom: 8px;
+  height: 33px;
+  line-height: 33px;
+  font-size: 16px;
+  color: #333333;
+}
+
+.OrderPage .order_content .order_wrap .order_bottom .bottom_list .td_spe{
+  border-bottom: 1px solid #dddddd;
  
 }
-.OrderPage .order_content .order_list .white_line span{
-  display: inline-block;
-  background-color:#ea5413;
-  border-radius: 50%;
-  color:#fff;
-  width: 18px; 
-  height:18px;
-  text-align: center;
-  position: absolute;
-}
-.OrderPage .order_content .order_list .white_line .point_one{
-  left: -17px;
-  top: -7px;
-}
-.OrderPage .order_content .order_list .white_line .point_two{
-  left: 209px;
-  top: -7px;
-}
-.OrderPage .order_content .order_list .white_line .point_three{
-  left:  431px;
-  top: -7px;
-} 
-.OrderPage .order_content .order_list .white_line .point_four{
-  left: 653px;
-  top: -7px;
-}
-.OrderPage .order_content .order_list .white_line .point_five{
-  right:-17px;
-  top: -7px;
+.OrderPage .order_content .order_wrap .order_bottom .bottom_list .td_spe td{
+   color: #999999 !important;
 }
 
-.step_one{ width:12%;}
-.step_two{ width:38%;}
-.step_three{ width:63%;}
-.step_four{ width:88%;}
-.step_five{ width:100%;}
+.OrderPage .order_content .order_wrap .order_bottom .bottom_list tr input{
+  width: 1000px;
+  height: 48px;
+  border:1px solid #ea5413;
+}
+.OrderPage .order_content .order_wrap .order_bottom .price .span_one{
+  display: inline-block;
+  margin-left: 12px;
+  font-size: 16px;
+  color: #333;
+  padding-bottom: 18px;
+
+}
+.OrderPage .order_content .order_wrap .order_bottom .price .span_two{
+  font-size: 20px;
+  color: #ea5413;
+}
+
+
+.OrderPage .order_content .order_wrap .order_bottom .scan_place{
+ width: 188px;
+ margin: 0 auto;
+ margin-bottom: 100px;
+
+}
+.OrderPage .order_content .order_wrap .order_bottom .scan_place p{
+  text-align: center;
+  line-height: 30px;
+  font-size: 16px;
+  color: #333333;
+}
+
 </style>
